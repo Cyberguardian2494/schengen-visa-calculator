d0085dc4c09e93d29b8cc12159453de8e0d0320d
Cyberguardian2494 <anson.anson2494@gmail.com>
2026-01-07 13:37:42 +0000

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/schengen-visa-calculator/sw.js', { scope: '/schengen-visa-calculator/' })})}
